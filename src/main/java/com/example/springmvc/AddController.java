7eb976d41330c65c896af42652c743bd32710e3e
NitheshKannaS <nitheshkanna23@gmail.com>
2023-02-16 12:58:05 +0530

    Tried Spring MVC

---
+++ src/main/java/com/example/springmvc/AddController.java
@@ -0,0 +1,13 @@
+package com.example.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController {
+    @RequestMapping("add")
+    public String add(){
+        return "display.jsp";
+    }
+}
